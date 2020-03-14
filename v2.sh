b4bd6333864f3a597b071aa6dfb000fd62e74bc3
St4ri0nS <62130639+Starions@users.noreply.github.com>
2020-03-14 10:03:32 +0700

    Create v2.sh

---
+++ v2.sh
@@ -0,0 +1,148 @@
+#!/system/bin/bash
+
+clear
+sleep 2
+echo "\033[1;32mTOOLS INI SUDAH DI UPDATE YA... DARI 5 TOOLS MENJADI 10 TOOLS"
+sleep 2
+echo "\033[1;33mDAN AKAN DI UPDATE SETIAP MINGGUNYA"
+sleep 2
+echo "\033[1;37mJANGAN LUPA BERI BINTANG GITHUB ADMIN :)"
+sleep 6
+clear
+echo "\033[1;34mMOHON TUNGGU SEBENTAR..."
+sleep 4
+echo "\033[1;35mMEMUAT TOOLS..."
+sleep 2
+clear
+echo "\033[1;32m"
+date
+echo "   \033[1;31mGithub.com\033[1;33m/\033[1;32mStarions \033[1;34m| \033[1;31mUpdate tgl \033[1;33m13\033[1;33m maret"
+echo "    \033[1;32mCreate \033[1;33m:\033[1;31m 2020\033[1;34m  |\033[1;32m Author\033[1;33m :\033[1;32m Mr.St4ri0ns"
+echo "\033[1;32m<\033[1;33m══════════════════════════════════════════════\033[1;32m>"
+echo "   \033[1;35m      [\033[1;33m+\033[1;35m] \033[1;37mSTARIVER CYBER NETWORK\033[1;35m [\033[1;33m+\033[1;35m]"
+echo ""
+echo "\033[1;33m╔══╗╔══════════════════════════════╗╔════════════╗"
+echo "║\033[1;32mNO\033[33m║║ \033[1;32m        NAMA TOOLS          \033[1;33m ║║ \033[1;31m  STATUS\033[1;33m   ║"
+echo "╚════════════════════════════════════════════════╝"
+echo "\033[1;34m╔══╗╔═══════════════════════════╗     ╔═══════╗"
+echo "║\033[1;33m01\033[1;34m║║\033[1;32m TOMBOL SPESIAL     \033[1;34m       ║    ║\033[1;32m   ON \033[1;34m   ║"
+echo "╚══╝╚═══════════════════════════╝     ╚═══════╝"
+echo "\033[1;34m╔══╗╔═══════════════════════════╗     ╔═══════╗"
+echo "║\033[1;33m02\033[1;34m║║\033[1;32m SCAN IP WEBSITE    \033[1;34m       ║    ║\033[1;32m   ON \033[1;34m   ║"
+echo "╚══╝╚═══════════════════════════╝     ╚═══════╝"
+echo "\033[1;34m╔══╗╔═══════════════════════════╗     ╔═══════╗"
+echo "║\033[1;33m03\033[1;34m║║\033[1;32m DDOS ATTACK        \033[1;34m       ║    ║\033[1;32m   ON \033[1;34m   ║"
+echo "╚══╝╚═══════════════════════════╝     ╚═══════╝"
+echo "\033[1;34m╔══╗╔═══════════════════════════╗     ╔═══════╗"
+echo "║\033[1;33m04\033[1;34m║║\033[1;32m SPAM SMS My Im3    \033[1;34m       ║    ║\033[1;32m   ON \033[1;34m   ║"
+echo "╚══╝╚═══════════════════════════╝     ╚═══════╝"
+echo "\033[1;34m╔══╗╔═══════════════════════════╗     ╔═══════╗"
+echo "║\033[1;33m05\033[1;34m║║\033[1;32m SPAM SMS OYO HOTEL \033[1;34m       ║    ║\033[1;32m   ON \033[1;34m   ║"
+echo "╚══╝╚═══════════════════════════╝     ╚═══════╝"
+echo "\033[1;34m╔══╗╔═══════════════════════════╗     ╔═══════╗"
+echo "║\033[1;33m06\033[1;34m║║\033[1;32m DOWNLOAD BAHAN²    \033[1;34m       ║    ║\033[1;32m   ON \033[1;34m   ║"
+echo "╚══╝╚═══════════════════════════╝     ╚═══════╝"
+echo "\033[1;34m╔══╗╔═══════════════════════════╗     ╔═══════╗"
+echo "║\033[1;33m07\033[1;34m║║\033[1;32m SPAM CHAT V1\033[1;33m (Wa)  \033[1;34m       ║    ║\033[1;32m   ON \033[1;34m   ║"                
+echo "╚══╝╚═══════════════════════════╝     ╚═══════╝"
+echo "\033[1;34m╔══╗╔═══════════════════════════╗     ╔═══════╗"
+echo "║\033[1;33m08\033[1;34m║║\033[1;32m DOWNLOAD VIDIO FB  \033[1;34m       ║    ║\033[1;32m   ON \033[1;34m   ║"
+echo "╚══╝╚═══════════════════════════╝     ╚═══════╝"
+echo "\033[1;34m╔══╗╔═══════════════════════════╗     ╔═══════╗"
+echo "║\033[1;33m09\033[1;34m║║\033[1;32m DOWNLOAD BUG WA    \033[1;34m       ║    ║\033[1;32m   ON \033[1;34m   ║"
+echo "╚══╝╚═══════════════════════════╝     ╚═══════╝"
+echo "\033[1;34m╔══╗╔═══════════════════════════╗     ╔═══════╗"
+echo "║\033[1;33m10\033[1;34m║║\033[1;32m SPAM WA V2\033[1;33m (Unli)  \033[1;34m       ║    ║\033[1;32m   ON \033[1;34m   ║"
+echo "╚══╝╚═══════════════════════════╝     ╚═══════╝"
+echo "\033[1;35m"
+read -p "SELECT YOUR INPUT :" input
+
+
+if [ $input = "1" ]
+then
+        cd $HOME
+        git clone https://github.com/Starions/Tombol-Spesial
+        cd Tombol-Spesial
+        python2 spesial.py
+
+
+elif [ $input = "2" ]
+then
+echo "\033[1;37m█████████
+█▄█████▄█
+█\033[1;31m▼▼▼▼▼
+\033[1;37m█ \033[1;32mIP WEBSITE SCANNER
+\033[1;37m█\033[1;31m▲▲▲▲▲
+\033[1;37m█████████
+ ██   ██"
+
+echo "\033[1;34m═════════════════════════════════"
+echo " AUTHOR : Mr.St4ri0ns"
+echo " GITHUB : github.com/Starions"
+echo " VERSION: 1.0"
+echo "═════════════════════════════════"
+echo "\033[1;32m"
+read -p "MASUKAN NAMA WEBSITE :" web
+echo "SCANNING IP..." | lolcat
+sleep 3
+        cd $HOME
+        ping $web
+
+elif [ $input = "5" ]
+then
+        cd $HOME
+        git clone https://github.com/Starions/spam-oyo
+        cd spam-oyo
+        php so.php
+
+elif [ $input = "4" ]
+then
+        cd $HOME
+        git clone https://github.com/Starions/spam-im3
+        cd spam-im3
+        php IM3.php
+
+elif [ $input = "3" ]
+then
+        cd $HOME
+        git clone https://github.com/Starions/ddos-v1
+        cd ddos-v1
+        python3 ddos.py
+
+elif [ $input = "8" ]
+then
+        cd $HOME
+        git clone https://github.com/Starions/fb-vidio
+        cd fb-vidio
+        php fbd.php
+
+elif [ $input = "10" ]
+then
+        cd $HOME
+        git clone https://github.com/Starions/spam-unli
+        cd spam-unli
+        php spamu.php
+
+elif [ $input = "9" ]
+then
+        cd $HONE
+        git clone https://github.com/Starions/VirtexTools
+        cd VirtexTools
+        sh 1.sh
+
+elif [ $input = "7" ]
+then
+        cd $HOME
+        git clone https://github.com/Starions/spam-wa
+        cd spam-wa
+        python2 Spammer.py
+
+elif [ $pilih = "6" ]
+then
+        cd $HOME
+        pkg install git
+        pkg install php
+        pkg install python2
+        pkg install python
+        pkg install lolcat
+fi
